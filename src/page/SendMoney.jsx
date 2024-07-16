2492a8517c87c615a7a90803d254289557ea166d
knh-nehal <nasibhossain57@gmail.com>
2024-07-16 15:56:24 +0600

    Add: Send money page

--- src/page/SendMoney.jsx
+++ src/page/SendMoney.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-
 const SendMoney = () => {
-    return (
-        <div>
-            Money
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+  };
+  return (
+    <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+      <div className="p-4 sm:p-7">
+        <div className="text-center">
+          <h1 className="block text-2xl font-bold text-gray-800">Send Money</h1>
         </div>
-    );
+
+        <div className="mt-5">
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-y-4">
+              <div>
+                <label htmlFor="email" className="block text-sm mb-2">
+                  Phone No. / Email address
+                </label>
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="email"
+                    name="text"
+                    className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                    required
+                    aria-describedby="email-error"
+                  />
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+              >
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SendMoney;
